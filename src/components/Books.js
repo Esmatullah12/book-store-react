67aa0764389a775431fc71b773641e1965cba22c
Esmatullah <esmatqadeemi12@gmail.com>
2023-10-05 21:46:20 +0100

    Use react-redux for the book store appliction

--- src/components/Books.js
+++ src/components/Books.js
@@ -2,22 +2,15 @@ import React from 'react';
 import BookList from './BookList';
 import BookForm from './BookForm';
 
-const booksArray = [
-  { id: 1, title: 'TED Talks', author: 'Chris Anderson' },
-  { id: 2, title: 'Homo Deus', author: 'Noah Harari' },
-  { id: 3, title: 'Factfulness', author: 'Hans Rosling' },
-  { id: 4, title: 'Kite Runner', author: 'Khalid Hassani' },
-];
-
 const onDelete = (bookId) => {
   console.log(`Delete book with ID ${bookId}`);
 };
 
 const Books = () => (
-  <di>
-    <BookList books={booksArray} onDelete={onDelete} />
+  <div>
+    <BookList onDelete={onDelete} />
     <BookForm />
-  </di>
+  </div>
 );
 
 export default Books;
--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,16 +1,39 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+const initialState = {
+  booksData: [
+    {
+      item_id: '1',
+      title: 'The Great Gatsby',
+      author: 'John Smith',
+      category: 'Fiction',
+    },
+    {
+      item_id: '2',
+      title: 'Anna Karenina',
+      author: 'Leo Tolstoy',
+      category: 'Fiction',
+    },
+    {
+      item_id: '3',
+      title: 'The Selfish Gene',
+      author: 'Richard Dawkins',
+      category: 'Nonfiction',
+    },
+  ],
+};
+
 const bookSlice = createSlice({
   name: 'books',
-  initialState: [],
+  initialState,
   reducers: {
     addBook: (state, action) => {
       const newBook = action.payload;
-      state.push(newBook);
+      state.booksData = [...state.booksData, newBook];
     },
     removeBook: (state, action) => {
-      const bookIdRemove = action.payload;
-      return state.filter((book) => book.id !== bookIdRemove);
+      const bookItemIdRemove = action.payload;
+      state.booksData = state.booksData.filter((book) => book.item_id !== bookItemIdRemove);
     },
   },
 
